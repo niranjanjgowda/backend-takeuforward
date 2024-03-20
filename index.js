f38e74039099165350437f6b09841cb1ede4ae20
niranjanjgowda <niranjanjgowda1@gmail.com>
2024-03-21 00:40:06 +0530

    cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(
     {
-        origin: ['https://frontend-takeuforward.vercel.app/', 'http://localhost:3000']
+        origin: 'https://frontend-takeuforward.vercel.app'
     }
 ));
 
