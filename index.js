4686d1c96b56efc2e5f506a9ce961f4f732792ac
niranjanjgowda <niranjanjgowda1@gmail.com>
2024-03-21 00:34:31 +0530

    few changes

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ const express = require('express');
 const mysql = require('mysql');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const redis = require('redis');
+const axios = require('axios');
+
 
 dotenv.config();
 
 const app = express();
 const port = 3000;
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(
+    {
+        origin: ['https://frontend-takeuforward.vercel.app/', 'http://localhost:3000']
+    }
+));
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
@@ -19,41 +27,105 @@ const connection = mysql.createConnection({
     database: process.env.DB_NAME
 });
 
-app.post('/submit', (req, res) => {
-    
-    connection.connect((err) => {
-        if (err) throw err;
-        console.log('Connected to MySQL database');
-    });
-
-    const { username, language, stdin, sourceCode } = req.body;
-
-    const query = 'INSERT INTO submissions (username, language, stdin, source_code) VALUES (?, ?, ?, ?)';
-    const values = [username, language, stdin, sourceCode];
-
-    connection.query(query, values, (err, result) => {
-        if (err) throw err;
-        console.log(`Data inserted with ID: ${result.insertId}`);
-        res.send('Data received and saved successfully!');
-    });
-
-    connection.end();
+const redisClient = redis.createClient({
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT
+    }
 });
 
-app.get('/submissions', (req, res) => {
-    
-    connection.connect((err) => {
-        if (err) throw err;
-        console.log('Connected to MySQL database');
-    });
+redisClient.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to Redis');
+});
 
-    const query = 'SELECT * FROM submissions';
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySQL database');
+});
+
+app.get('/', (req, res) => {
+    res.send('Api live!');
+});
+
+
+app.post('/submit', async (req, res) => {
+    const { username, language, stdin, sourceCode, questionNumber } = req.body;
+    const languages = { "C++": 52, "Java": 62, "JavaScript": 63, "Python": 71 }
+    var postData = null
+
+    if (questionNumber === undefined || questionNumber === null || questionNumber === '') {
+        return res.status(400).send('Question number is not valid!');
+    }
+    else {
+        if (questionNumber == 1) {
+            postData = {
+                language_id: languages[language],
+                source_code: sourceCode,
+                stdin: stdin,
+                expected_output: "Hello world"
+            }
+        }
+    }
+
+    const options = {
+        method: 'POST',
+        url: 'https://judge0-ce.p.rapidapi.com/submissions',
+        params: {
+            base64_encoded: 'true',
+            fields: '*'
+        },
+        headers: {
+            'content-type': 'application/json',
+            'Content-Type': 'application/json',
+            'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+            'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
+        },
+        data: postData
+    };
+
+    const response = await axios.request(options);
+    const token = response.data.token;
+
+    const getoptions = {
+        method: 'GET',
+        url: `https://judge0-ce.p.rapidapi.com/submissions/${token}`,
+        params: {
+            base64_encoded: 'true',
+            fields: '*'
+        },
+        headers: {
+            'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+            'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const getresponse = await axios.request(getoptions);
+        //we can get whatever we want from the response
+        const compilelog = getresponse.data.status.description;
+
+        const query = 'INSERT INTO submissions (username, language, stdin, source_code,token, compilelog) VALUES (?, ?, ?, ?, ?, ?)';
+        const values = [username, language, stdin, sourceCode, token, compilelog];
+
+        connection.query(query, values, (err, result) => {
+            if (err) throw err;
+            console.log(`Data inserted with ID: ${result.insertId}`);
+        });
+        res.send(getresponse.data);
+
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+app.get('/submissions', async (req, res) => {
+    const query = 'SELECT username, language, stdin, source_code, compilelog  FROM submissions';
     connection.query(query, (err, results) => {
         if (err) throw err;
         res.send(results);
     });
-
-    connection.end();
 });
 
 app.listen(port, () => {
